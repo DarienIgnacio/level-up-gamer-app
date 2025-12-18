5e2e8baeaf72a2ebeeacd66f5b3a52116c083884
DarienIgnacio <dari.urrutia@duocuc.cl>
2025-12-18 03:38:41 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    // aquí puedes agregar métodos personalizados si quieres
-}
